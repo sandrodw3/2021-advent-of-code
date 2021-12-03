61ef37c47b577312542c259d86af79e039447340
Sandro Chinea <sandrochinea@gmail.com>
2021-12-03 11:18:18 +0000

    Add solution for Puzzle 1 Part 1

---
+++ puzzle-1-1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const data = fs
+  .readFileSync('./puzzle-1-input.txt', {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+  .split('\n');
+
+console.log(
+  'Number of increasing measurements: ',
+  getIncreasingMeasurements(data)
+);
+
+function getIncreasingMeasurements(measurements) {
+  return measurements.filter(
+    (measurement, index) =>
+      parseInt(measurement) > parseInt(measurements[index - 1])
+  ).length;
+}
