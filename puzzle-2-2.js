0fd7ae815da95bfa0d361bbf583ac23c2530aa15
Sandro Chinea <sandrochinea@gmail.com>
2021-12-10 09:59:47 +0000

    Add solution for Puzzle 2 Part 2

---
+++ puzzle-2-2.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const data = fs
+  .readFileSync('./puzzle-2-input.txt', {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+  .split('\n');
+
+console.log(
+  'Result of multiplying horizontal position by final depth: ',
+  getFinalPosition(data)
+);
+
+function getFinalPosition(movements) {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  movements.forEach((movement) => {
+    const [, displacement] = movement.split(' ');
+
+    if (movement.includes('up')) {
+      aim -= parseInt(displacement);
+    } else if (movement.includes('down')) {
+      aim += parseInt(displacement);
+    } else if (movement.includes('forward')) {
+      horizontal += parseInt(displacement);
+      depth += parseInt(displacement) * aim;
+    }
+  });
+
+  return horizontal * depth;
+}
